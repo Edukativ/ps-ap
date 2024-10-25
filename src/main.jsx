09e54a48b7acdf232f7d3804c50f403a6739949f
Edukativ <86229739+Edukativ@users.noreply.github.com>
2024-10-25 16:40:48 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
+import { HashRouter } from "react-router-dom"
+
+ReactDOM.render(
+    <HashRouter><App /></HashRouter>,
+    document.getElementById('root')
+);
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
