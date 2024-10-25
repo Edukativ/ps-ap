2cf066304d3933b842d57673b2765e8d0cc215db
Edukativ <86229739+Edukativ@users.noreply.github.com>
2024-10-25 17:16:48 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ps-ap/',
+  base: 'https://edukativ.github.io/ps-ap/',
   plugins: [react()],
 })
