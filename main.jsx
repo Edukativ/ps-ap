6b9280f2552c3c94914e0af54228c2bd86470685
Edukativ <86229739+Edukativ@users.noreply.github.com>
2024-10-25 16:39:58 +0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import { Provider } from 'react-redux'
+import store from './store/store.js'
+import { HashRouter } from "react-router-dom"
+
+ReactDOM.render(
+    <HashRouter><App /></HashRouter>,
+    document.getElementById('root')
+);
+
+createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+)
