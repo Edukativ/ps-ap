14465c67001e1878dd73d903b3ba56e8d609502a
Edukativ <86229739+Edukativ@users.noreply.github.com>
2024-10-25 17:28:58 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
-import.meta.env.BASE_URL
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    ssr({
-    baseAssets: 'https://edukativ.github.io/',
-    baseServer: '/ps-ap/'
-    })
-  ],
+  plugins: [react()],
 })
-
